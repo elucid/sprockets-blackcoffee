91209460334180287e73e250f5083bd8a4a0987a
Justin Giancola <justin.giancola@gmail.com>
2014-09-18 17:10:45 -0400

    Whitespace

--- lib/sprockets-es6-coffee/template.rb
+++ lib/sprockets-es6-coffee/template.rb
@@ -3,7 +3,6 @@ require 'tilt/coffee'
 module Sprockets
   module Es6Coffee
     class Template < Tilt::CoffeeScriptTemplate
-    
       def evaluate(scope, locals, &block)
         if scope.pathname.to_s.match /\.es6\.coffee/
           options[:bare] = true
@@ -11,9 +10,6 @@ module Sprockets
 
         super
       end
-    
     end
-    
   end
 end
-
